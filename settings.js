7da102fba3f44bd193a514d7a5c907a036483aaf
Botaktif1810 <109157291+Botaktif1810@users.noreply.github.com>
2022-08-13 15:04:57 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6285763590930','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6285763590930'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6285763590930' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['79811719576'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['79811719576'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '79811719576' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Leo new' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'отлично' //ubah jadi nama mu, note tanda ' gausah di hapus!
 global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Leo official' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.footer = 'Bot official' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtu.be/dQw4w9WgXcQ'
 global.ig = 'https://instagram.com/leo387exe'
-global.mygc = 'https://chat.whatsapp.com/JVwxKsFk5WQE2WsmBzJN4B'
+global.mygc = 'not available'
 global.myweb = 'https://s.id/chikabot'
 global.email = 'si797410@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
 global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Leo anak esemka' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.author = 'сделано в россии' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -52,7 +52,7 @@ global.sessionName = 'session'
 //terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
-free: 15,
+free: 100,
 }
 
 //—————「 Set Image 」—————//
